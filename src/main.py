d53caacb46e1f76ae90b8dd9a8bb4efc72f40955
Egor Matveev <egormatveev0505@gmail.com>
2024-10-10 17:52:31 +0300

    initial project setup

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from vk_api_wrapper import VKApiWrapper
+from file_processor import FileProcessor
+
+def main():
+    # Здесь будет основная логика программы
+    pass
+
+if __name__ == "__main__":
+    main()
